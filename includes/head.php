bcde4a4af0d2468807a8b4234191ff45631eebf3
pham-anh <pham-anh@users.noreply.github.com>
2016-06-05 00:22:32 +0700

    debug server protocol

--- includes/head.php
+++ includes/head.php
@@ -2,15 +2,18 @@
 ini_set('display_errors','On'); 
 error_reporting(E_ALL);
 
+var_dump($_SERVER);
+
 if (strpos($_SERVER['SERVER_PROTOCOL'], 'HTTPS') == FALSE) {
     $protocol = 'http://';
 } else {
     $protocol = 'https://';
 }
 $main_dir = $protocol . $_SERVER['SERVER_NAME'];
+
 ?>
 <title>News system</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<?php echo $main_dir . 'bootstrap/css/bootstrap.min.css' ?>">
-<link rel="stylesheet" href="<?php echo $main_dir . 'bootstrap/css/mystyle.css' ?>">
+<link rel="stylesheet" href="<?php echo $main_dir . '/bootstrap/css/bootstrap.min.css' ?>">
+<link rel="stylesheet" href="<?php echo $main_dir . '/bootstrap/css/mystyle.css' ?>">
