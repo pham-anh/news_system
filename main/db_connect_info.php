06eaf336cd2fcf3aba2cebe5df860fc9d5198581
pham-anh <anhpham89@gmail.com>
2016-06-04 23:45:59 +0700

    Merge branch 'master' of https://github.com/pham-anh/news_system
    # Conflicts:
    #	main/db_connect_info.php
    #	main/post_list.php

--- main/db_connect_info.php
+++ main/db_connect_info.php
@@ -11,14 +11,14 @@ function run_sql($queryString) {
     $db_connect = mysqli_connect(HOST, U, P, DB);
     //Kiểm tra kết nối  -- Trả về false (bool)  nếu kết nối thất bại
     if ($db_connect === false) { //Nếu không kết nối được trả về báo lỗi
-        echo "<h3 class='text-danger'>Connection error. Please try again later.</h3>";
+        echo "<h3 class='text-danger'>Connection failed." .mysqli_connect_error(). "</h3>";;
         exit;
     } else {
         //Nếu kết nối thành công -- Chạy câu truy vấn: trả về false nếu chạy bị lỗi, ngược lại: resource hoặc true (bool) tùy câu truy vấn
         $query_exec = mysqli_query($db_connect, $queryString);
         //Nếu bị lỗi --> trả về báo lỗi
         if ($query_exec === false) {
-            echo "<h3 class='text-danger'>Connection error. Please try again later.</h3>";
+            echo "<h3 class='text-danger'>Query failed." .mysqli_error($db_connect). "</h3>";
             exit;
         } else { //Ngược lại: trả về kết quả chạy câu sql thành công
             return $query_exec;
