cf03290a5d75d40c05eb96ad1611476621266837
pham-anh <anhpham89@gmail.com>
2016-06-05 14:28:39 +0700

    fix #2

--- post_list.php
+++ post_list.php
@@ -24,7 +24,7 @@
                 // put your code here
                 //Display all posts available in DB, only logged in people can see this
                 require_once 'db_connect_info.php';
-                $sql = "SELECT *, unix_timestamp(post.updated_at) as updated_timestamp FROM post ORDER BY updated_at DESC";
+                $sql = "SELECT *, unix_timestamp(post.updated_at) as updated_timestamp, unix_timestamp(post.created_at) as created_timestamp FROM post ORDER BY updated_at DESC";
                 $posts = run_sql($sql);
                 //var_dump($posts);
 
@@ -32,21 +32,29 @@
                     if (!isset($_GET['post_id'])) {
                         foreach ($posts as $post) {
                             //var_dump($post);
-                            $time_ago = time() - $post['updated_timestamp'];
-                            //echo $time_ago;
-
+                            if (is_null($post['updated_timestamp'])) {
+                                $time_ago = time() - $post['created_timestamp'];
+                                $detailed_time = $post['created_timestamp'];
+                            } else {
+                                $time_ago = time() - $post['updated_timestamp'];
+                                $detailed_time = $post['updated_timestamp'];
+                            }
                             if ($time_ago < 60 * 60) {
                                 $time_display = 'About ' . floor($time_ago / 60) . ' minutes ago';
                             } else if ($time_ago < 60 * 60 * 24) {
                                 $time_display = 'About ' . floor($time_ago / 3600) . ' hours ago';
                             } else {
-                                $time_display = date('H:i Y-m-d', time($post['updated_at']));
+                                if (is_null($post['updated_timestamp'])) {
+                                    $time_display = date('H:i Y-m-d', time($post['created_timestamp']));
+                                } else {
+                                    $time_display = date('H:i Y-m-d', time($post['updated_timestamp']));
+                                }
                             }
                             ?>
                             <div>
 
                                 <h2><?php echo $post['subject'] ?></h2>
-                                <p title="<?php echo date('D j M Y H:i', $post['updated_timestamp']) ?>"><span class="glyphicon glyphicon-time" aria-hidden="true"></span><span class="label label-success"> <?php echo $time_display ?></span></p>
+                                <p title="<?php echo date('D j M Y H:i', $detailed_time) ?>"><span class="glyphicon glyphicon-time" aria-hidden="true"></span><span class="label label-success"> <?php echo $time_display ?></span></p>
 
                                 <?php if ($post['img'] != '') {
                                     ?>
@@ -73,7 +81,7 @@
                                 ?>
                                 <!--<div class="">-->
                                 <h2><?php echo $post['subject'] ?></h2>
-                                <p><span class="glyphicon glyphicon-time" aria-hidden="true"></span> <?php echo date('D j M Y H:i', $post['updated_timestamp']) ?> &nbsp;&nbsp;&nbsp;&nbsp;<button class="small"><a href="post_edit.php?post_id=<?php echo $post_id ?>">Edit post</a></button></p>
+                                <p><span class="glyphicon glyphicon-time" aria-hidden="true"></span> <?php echo date('D j M Y H:i', $detailed_time) ?> &nbsp;&nbsp;&nbsp;&nbsp;<button class="small"><a href="post_edit.php?post_id=<?php echo $post_id ?>">Edit post</a></button></p>
                                 <?php if ($post['img'] != '') {
                                     ?>
                                     <center><image src="<?php echo $post['img'] ?>" alt="image" class="img-rounded img-responsive" width="25%" length="25%" /></center><br>
